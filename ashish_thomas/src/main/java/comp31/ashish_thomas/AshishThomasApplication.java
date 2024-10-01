746ae1a7fcaee1e09463480e6ed20dda6d98838f
AshishThomas1804 <ashish.thomas@Student.SL.On.Ca>
2024-10-01 01:32:47 -0400

    Initial Commit

---
+++ ashish_thomas/src/main/java/comp31/ashish_thomas/AshishThomasApplication.java
@@ -0,0 +1,13 @@
+package comp31.ashish_thomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshishThomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshishThomasApplication.class, args);
+	}
+
+}
---
+++ ashish_thomas/src/test/java/comp31/ashish_thomas/AshishThomasApplicationTests.java
@@ -0,0 +1,13 @@
+package comp31.ashish_thomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshishThomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
